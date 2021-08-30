791802874fcc2a7290c242af9afcc5ca5201484d
Shubham Mehar <shubhammehar101@gmail.com>
2021-08-30 17:20:35 +0530

    commit

--- blog_app/config.py
+++ blog_app/config.py
@@ -2,8 +2,8 @@ import os
 
 
 class Config():
-    SQLALCHEMY_DATABASE_URI  = "sqlite:///blog_app.db"
-    SECRET_KEY  = "5ntw35eq24eu7oysftbhbncfy6fxdb65bgshwcjtvvd4"
+    SQLALCHEMY_DATABASE_URI  = os.environ.get('SQLALCHEMY_DATABASE_URI')
+    SECRET_KEY  = os.environ.get('SECRET_KEY')
     MAIL_SERVER ='smtp.google.com'
     MAIL_PORT =587
     MAIL_USE_TLS =True
